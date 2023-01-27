0542451d06dbc5c948985a0412a1df3a91279314
Isaac Takiy <58862891+Isaac-web@users.noreply.github.com>
2023-01-27 05:38:33 +0000

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const arr = [
+  const items = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
-  return (arr);
+  return (items);
 }
