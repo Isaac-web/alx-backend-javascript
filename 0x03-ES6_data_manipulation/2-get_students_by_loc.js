41c77024e9205a4ef9f27704ec0be3ffa04d69cc
Isaac-web <kanytakiy@gmail.com>
2023-02-01 04:37:32 +0000

    Fix Task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
 export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) return [];
-  return students.filter((student) => student.city === city);
+  return students.filter((student) => student.location === city);
 }
